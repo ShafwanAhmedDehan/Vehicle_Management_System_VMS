da031f2838bd1b053e5e03125b5437550a9a2eb2
Shafwan Ahmed Dehan <88941317+ShafwanAhmedDehan@users.noreply.github.com>
2023-06-25 17:53:45 +0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+	
+?>
+<!DOCTYPE html>
+<html>
+<center>
+<body style="background-image: linear-gradient(to right, #0E5CAD,#79F1A4);">
+	<div><header style="position: fixed; left: 0; top: 0; width:100%"><?php include "Top/Top.html"?> </header></div>
+	<br><br><br><br><br><br><br>
+<button onclick="document.location='http://localhost/WebTechProject/View/Login.php'" style="background-color: skyblue; border-radius: 5px; text-align: center; width: 350px; border: none; font-size: 17px; padding: 10px; cursor: pointer;"><b>Md Shafwan Ahmed Dehan(19-39302-1)</b></button>
+	<br><br><br>
+	<button onclick="document.location='http://localhost/WebTechProject/View/Login.php'" style="background-color: skyblue; border-radius: 5px; text-align: center; width: 300px; border: none; font-size: 17px; padding: 10px; cursor: pointer;"><b>Sadia Islam Zerin(20-43741-2)</b></button>
+   <br><br><br>
+  <button onclick="document.location='http://localhost/WebTechProject/View/Login.php'" style="background-color: skyblue; border-radius: 5px; text-align: center; width: 300px; border: none; font-size: 17px; padding: 10px; cursor: pointer;"><b>Md Tanvir Hossen(20-42488-1)</b></button>
+	<div><footer style="position: fixed; left: 0; bottom: 0; width: 100%;" > <?php include "Bottom/End.html" ?> </footer></div>
+</body>
+</center>
+</html>
